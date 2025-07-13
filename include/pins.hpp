98b74de25834c73af7b48aa8099a3ceab514828a
Martin Kubovčík <markub3327@gmail.com>
2025-07-13 14:20:06 +0200

    update

--- include/pins.hpp
+++ include/pins.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#define PUMP_PIN            21
+#define PUMP_PIN             2
 
 #define SOIL_MOISTURE_1     A0
 #define SOIL_MOISTURE_2     A1
 #define SOIL_MOISTURE_3     A2
 #define SOIL_MOISTURE_4     A3
-#define SOIL_MOISTURE_5     A4
-#define SOIL_MOISTURE_6     A5
+#define SOIL_MOISTURE_5     A6
+#define SOIL_MOISTURE_6     A7
